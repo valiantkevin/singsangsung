0e85fa70fb409952d70475b58830c372095766ad
Kevin Valiant <kevin.valiant@ti.ukdw.ac.id>
2017-04-19 10:19:38 +0700

    setting file added

---
+++ SingSangSung/FileModule.vb
@@ -0,0 +1,7 @@
+﻿Imports System.IO
+Module FileModule
+    Public path As String = My.Application.Info.DirectoryPath
+    Public file As String = "setting.txt"
+    Public numberOfRoom As Integer = 25
+
+End Module
